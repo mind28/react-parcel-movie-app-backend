cead643d2719f217ae76eb3b46b3d8e7c1c53791
mind28 <96925869+mind28@users.noreply.github.com>
2022-05-21 15:03:47 -0400

    getting ready to deploy

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -29,11 +30,19 @@ let movieSchema = mongoose.Schema({
     password: {type: String, required: true},
     email: {type: String, required: true},
     birthday: Date,
-    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'movie' }]
   });
+
+  userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+  };
   
-  let Movie = mongoose.model('Movie', movieSchema);
-  let User = mongoose.model('User', userSchema);
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };  
+  
+  let Movie = mongoose.model('movie', movieSchema);
+  let User = mongoose.model('user', userSchema);
   
   module.exports.Movie = Movie;
   module.exports.User = User;
